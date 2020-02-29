0883e5edd28d47574c7a50a420682d1dd2e85329
David Domkář <dave.domkar@gmail.com>
2020-02-29 17:10:11 +0100

    feat: use dartex from pub.dev

--- lib/bundles/transform/transform_bundle.dart
+++ lib/bundles/transform/transform_bundle.dart
@@ -1,5 +1,9 @@
+import 'package:fluttershy/bundles/transform/components/transform_component.dart';
 import 'package:fluttershy/foundation/bundle.dart';
 
 class TransformBundle extends Bundle {
-  TransformBundle() : super([], []);
+  TransformBundle()
+      : super([
+          TransformComponent,
+        ], []);
 }
