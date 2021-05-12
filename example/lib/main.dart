c6a667d8e447fa829cb9f525adb90dfc8760c5d3
David Domkář <dave.domkar@gmail.com>
2021-05-12 21:11:45 +0200

    refactor(engine): migrate to null safety, remove context

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,16 +1,25 @@
 import 'dart:math';
 
-import 'package:flutter/material.dart'
-    hide State, Transform, Size, PointerMoveEvent;
+import 'package:flutter/material.dart' hide State, Transform, Size, PointerMoveEvent;
 import 'package:fluttershy/fluttershy.dart';
 import 'package:fluttershy/events/pointer_event.dart';
 
 void main() => runApp(MyApp());
 
+class ExampleContext {
+  final Paint paint = Paint()..color = Colors.amber;
+
+  double time = 0;
+  double positionX = 200;
+  double positionY = 200;
+}
+
+final context = ExampleContext();
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext _context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -20,38 +29,22 @@ class MyApp extends StatelessWidget {
         resizeToAvoidBottomInset: false,
         body: Container(
           margin: EdgeInsets.all(200),
-          child: FluttershyWidget(
-            contextBuilder: () => ExampleContext(),
-            event: (context, event) {
+          child: Fluttershy(
+            event: (event) {
               if (event.type == PointerMoveEvent) {
                 context.positionX = (event as PointerMoveEvent).position.x;
-                context.positionY = (event as PointerMoveEvent).position.y;
+                context.positionY = (event).position.y;
               }
             },
-            update: (context, deltaTime) {
+            update: (deltaTime) {
               context.time += deltaTime;
             },
-            render: (context, canvas) {
-              canvas.drawOval(
-                  Rect.fromCenter(
-                      center: Offset(context.positionX,
-                          context.positionY + sin(context.time * 5) * 20),
-                      width: 200,
-                      height: 200),
-                  context.paint);
+            render: (canvas) {
+              canvas.drawOval(Rect.fromCenter(center: Offset(context.positionX, context.positionY + sin(context.time * 5) * 20), width: 200, height: 200), context.paint);
             },
-            backgroundColor: Colors.pink,
           ),
         ),
       ),
     );
   }
-}
-
-class ExampleContext extends Context {
-  final Paint paint = Paint()..color = Colors.amber;
-
-  double time = 0;
-  double positionX = 200;
-  double positionY = 200;
 }
