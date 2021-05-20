14813ee9eb07a87e58f186928eedb13612f1d3e5
David Domkář <dave.domkar@gmail.com>
2021-05-20 14:38:19 +0200

    feat(sprite-batch) make rendering faster with caching and using drawRawAtlas

--- lib/sprite_batch.dart
+++ lib/sprite_batch.dart
@@ -1,10 +1,11 @@
+import 'dart:typed_data';
 import 'dart:ui';
 
 import 'texture.dart';
 import 'sprite.dart';
 
 class SpriteBatch {
-  final maxSprites = 5000;
+  static const maxSprites = 4000;
 
   int index = 0;
 
@@ -12,8 +13,8 @@ class SpriteBatch {
 
   Texture? texture;
 
-  final _transforms = <RSTransform>[];
-  final _sources = <Rect>[];
+  final _rawTransforms = Float32List(maxSprites * 4);
+  final _rawSources = Float32List(maxSprites * 4);
 
   void render(Canvas canvas, Sprite sprite) {
     if (sprite.texture != texture || index >= maxSprites) {
@@ -22,8 +23,22 @@ class SpriteBatch {
 
     texture = sprite.texture;
 
-    _transforms.add(sprite.transform);
-    _sources.add(sprite.rect);
+    final rect = sprite.rect;
+
+    final int index0 = index * 4;
+    final int index1 = index0 + 1;
+    final int index2 = index0 + 2;
+    final int index3 = index0 + 3;
+
+    _rawTransforms[index0] = sprite.scos;
+    _rawTransforms[index1] = sprite.ssin;
+    _rawTransforms[index2] = sprite.tx;
+    _rawTransforms[index3] = sprite.ty;
+
+    _rawSources[index0] = rect.left;
+    _rawSources[index1] = rect.top;
+    _rawSources[index2] = rect.right;
+    _rawSources[index3] = rect.bottom;
 
     index++;
   }
@@ -31,10 +46,10 @@ class SpriteBatch {
   void flush(Canvas canvas) {
     if (texture == null) return;
 
-    canvas.drawAtlas(texture!.image, _transforms, _sources, null, null, null, paint);
+    canvas.drawRawAtlas(texture!.image, _rawTransforms, _rawSources, null, null, null, paint);
 
-    _transforms.clear();
-    _sources.clear();
+    _rawTransforms.fillRange(0, _rawTransforms.length, 0.0);
+    _rawSources.fillRange(0, _rawSources.length, 0.0);
 
     index = 0;
   }
