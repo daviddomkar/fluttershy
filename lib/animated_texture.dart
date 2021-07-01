894c9968f0980bdca9b0e281ca0e4c28410aae63
David Domkář <dave.domkar@gmail.com>
2021-07-01 19:41:32 +0200

    feat(engine): allow non-looping animated textures

--- lib/animated_texture.dart
+++ lib/animated_texture.dart
@@ -2,16 +2,17 @@ import 'texture.dart';
 
 class AnimatedTexture extends Texture {
   double frameTime;
+  bool loop;
 
   final List<Texture> _frames;
 
   double _currentFrameTime;
   int _currentFrameIndex;
 
-  AnimatedTexture.fromTextures(List<Texture> frames, {this.frameTime = 1 / 60})
+  AnimatedTexture.fromTextures(List<Texture> frames, {this.frameTime = 1 / 60, this.loop = false})
       : assert(frames.isNotEmpty),
         _frames = frames,
-        _currentFrameTime = 0.0,
+        _currentFrameTime = 0,
         _currentFrameIndex = 0,
         super.fromImage(
           frames[0].image,
@@ -25,19 +26,36 @@ class AnimatedTexture extends Texture {
     _currentFrameTime += deltaTime;
 
     while (_currentFrameTime > frameTime) {
-      _currentFrameTime -= frameTime;
-
       if (_currentFrameIndex != _frames.length - 1) {
-        _currentFrameIndex++;
-      } else {
-        _currentFrameIndex = 0;
-      }
+        _currentFrameTime -= frameTime;
 
-      image = _frames[_currentFrameIndex].image;
-      position = _frames[_currentFrameIndex].position;
-      size = _frames[_currentFrameIndex].size;
-      trimOffset = _frames[_currentFrameIndex].trimOffset;
-      trimSize = _frames[_currentFrameIndex].trimSize;
+        _currentFrameIndex++;
+
+        image = _frames[_currentFrameIndex].image;
+        position = _frames[_currentFrameIndex].position;
+        size = _frames[_currentFrameIndex].size;
+        trimOffset = _frames[_currentFrameIndex].trimOffset;
+        trimSize = _frames[_currentFrameIndex].trimSize;
+      } else if (loop) {
+        _currentFrameTime -= frameTime;
+
+        _currentFrameIndex = 0;
+
+        image = _frames[_currentFrameIndex].image;
+        position = _frames[_currentFrameIndex].position;
+        size = _frames[_currentFrameIndex].size;
+        trimOffset = _frames[_currentFrameIndex].trimOffset;
+        trimSize = _frames[_currentFrameIndex].trimSize;
+      } else {
+        break;
+      }
     }
   }
+
+  void reset() {
+    _currentFrameTime = 0;
+    _currentFrameIndex = 0;
+  }
+
+  bool get isCompleted => !loop && _currentFrameIndex == _frames.length - 1 && _currentFrameTime >= frameTime;
 }
